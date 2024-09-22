9b8a71d7ce603a3142d624ff8ba36c0d8f245ff3
Aishwarya <aishwarya.kandavel@gmail.com>
2024-09-22 08:08:12 +0530

    BrowserStack integration with Github and Jenkins

--- src/test/java/test/BrowserStackTest.java
+++ src/test/java/test/BrowserStackTest.java
@@ -1,6 +1,11 @@
 package test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -9,8 +14,23 @@ public class BrowserStackTest {
 	WebDriver driver;
 	
 	@BeforeMethod
-	public void initiateDriverInstance() {
-		driver = new ChromeDriver();
+	public void initiateDriverInstance() throws MalformedURLException {
+		String username = System.getenv("BROWSERSTACK_USERNAME");
+		String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+		String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
+		String local = System.getenv("BROWSERSTACK_LOCAL");
+		String localidentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
+
+		MutableCapabilities capabilities = new MutableCapabilities();
+		capabilities.setCapability("browserstack.idleTimeout", "300"); // Setting idle timeout to 5 minutes
+		HashMap<String, Object> browserstackOptions = new HashMap<String, Object>();
+		browserstackOptions.put("sessionName", "BStack Build Name: " + buildName);
+		browserstackOptions.put("local", local);
+		browserstackOptions.put("localIdentifier", localidentifier);
+		browserstackOptions.put("seleniumVersion", "4.0.0");
+		capabilities.setCapability("bstack:options", browserstackOptions);
+
+		driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
 		driver.manage().window().maximize();
 	}
 	@AfterMethod
@@ -27,14 +47,14 @@ public class BrowserStackTest {
 		driver.get("https://rahulshettyacademy.com/client/");
 		System.out.println(driver.getTitle());
 	}
-//	@Test
-//	public void test3() {
-//		driver.get("https://rahulshettyacademy.com/upload-download-test/index.html");
-//		System.out.println(driver.getTitle());
-//	}
-//	@Test
-//	public void test4() {
-//		driver.get("https://rahulshettyacademy.com/angularpractice/");
-//		System.out.println(driver.getTitle());
-//	}
+	@Test
+	public void test3() {
+		driver.get("https://rahulshettyacademy.com/upload-download-test/index.html");
+		System.out.println(driver.getTitle());
+	}
+	@Test
+	public void test4() {
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		System.out.println(driver.getTitle());
+	}
 }
